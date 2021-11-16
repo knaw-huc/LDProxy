331ab15174fb466711fd48721cc21fb95845b45f
meindertkroese <>
2021-11-16 11:06:19 +0100

    adjust pom and module-info

--- recipe/src/main/java/module-info.java
+++ recipe/src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module nl.clariah.recipe {
+  requires Saxon.HE;
+  requires SaxonUtils;
   exports nl.clariah.recipe;
-  provides nl.clariah.recipe.Cat;
-  provides nl.clariah.recipe.Redirect;
+  provides nl.clariah.recipe.Cat
+      with nl.clariah.recipe.Cat;
+  provides nl.clariah.recipe.Redirect
+      with nl.clariah.recipe.Redirect;
 }
